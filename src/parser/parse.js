c5c3a733e05b5bb75f7eb6a951e7fce5bed3571f
Luc Patiny <luc@patiny.com>
2019-05-11 08:22:28 +0200

    clean parser code

--- src/parser/parse.js
+++ src/parser/parse.js
@@ -37,12 +37,6 @@ function parse(data, kind, isService = false, isRequest = false) {
     throw new Error('parse: Not a service or message');
   }
 
-  let unionDidPreceed = 0;
-  let extractedUnionType = -1;
-  let unionTagCount = 0;
-  let voidCount = 0;
-  let unionTagValueName = '';
-
   for (let variable of transfer.variables) {
     let currentResult = processVariable(bigValue, variable, from);
     if (variable.name) {
@@ -52,55 +46,6 @@ function parse(data, kind, isService = false, isRequest = false) {
       }
     }
     from = currentResult.from;
-
-    /*
-    if (variable.kind === 'void') {
-      variable.name = `${variable.kind}${voidCount}`;
-      voidCount++;
-    } else if (
-      kinds[variable.kind] &&
-      kinds[variable.kind].type === 'message'
-    ) {
-      variable = kinds[variable.kind].message.variables[0];
-    } else if (kinds[variable.kind] && kinds[variable.kind].type === 'union') {
-      unionDidPreceed = 1;
-      variable.bits = Math.ceil(
-        Math.log2(kinds[variable.kind].message.variables.length)
-      );
-      unionTagValueName = variable.name;
-      variable.name = `${variable.kind}${unionTagCount}`;
-      variable.kind = 'unionTag'; // rename kind to explicit name. must succeed bit length calculation
-      unionTagCount++;
-    }
-    from = processVariable(bigInt, variable, from, result);
-    // very implicit, FIXME
-    if (result.name) {
-      result.nameStr = nameToString(result.name);
-    }
-
-    if (result.key) {
-      result.keyStr = nameToString(result.key);
-    }
-
-    if (unionDidPreceed) {
-      unionDidPreceed = 0;
-      extractedUnionType = result[variable.name];
-
-      let name = variable.name.substr(0, variable.name.length - 1);
-      let unionType = kinds[name].message.variables[1];
-      if (unionType.bits) {
-        variable.bits = unionType.bits;
-      } else {
-        variable.bits = 0; // 0 on Empty type
-      }
-      variable.kind = unionType.kind;
-      variable.name = unionTagValueName;
-
-      from = processVariable(bigInt, variable, from, result);
-
-      extractedUnionType = -1;
-    }
-    */
   }
 
   return result;
