bbe63f2186f9b242c0f359afb0436ad2d23d6d8b
Luc Patiny <luc@patiny.com>
2019-05-11 08:19:23 +0200

    fix correctly int serializer

---
+++ src/serializer/serialize.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* global BigInt */
+
+const processVariable = require('./processVariable');
+/**
+ *
+ * @param {*} data Buffer
+ * @param {*} kind
+ */
+function serialize(data, kind, isService = false, isRequest = false) {}
+
+module.exports = serialize;
--- src/serializer/util/serializeInt.js
+++ src/serializer/util/serializeInt.js
@@ -9,27 +9,29 @@
 */
 
 const n0 = BigInt(0);
+const n1 = BigInt(1);
 const n8 = BigInt(8);
 const n255 = BigInt(255);
 
 function serializeInt(value, nbBits, unsigned = false) {
+  value = BigInt(value);
   if (unsigned) {
-    let maxValue = (1 << nbBits) - 1;
+    let maxValue = (n1 << BigInt(nbBits)) - n1;
     if (value > maxValue) value = maxValue;
-    if (value < 0) value = 0;
+    if (value < n0) value = n0;
     return littleEndian(value, nbBits);
   }
   // signed
-  if (value >= 0) {
-    let maxValue = (1 << (nbBits - 1)) - 1;
+  if (value >= n0) {
+    let maxValue = (n1 << BigInt(nbBits - 1)) - n1;
     if (value > maxValue) value = maxValue;
     return littleEndian(value, nbBits);
   }
   // negative number
-  const minValue = (1 << (nbBits - 1)) * -1;
-
+  const minValue = (n1 << (BigInt(nbBits) - n1)) * -n1;
   if (value < minValue) value = minValue;
-  return littleEndian(value & ((1 << nbBits) - 1), nbBits);
+
+  return littleEndian(value & ((n1 << BigInt(nbBits)) - n1), nbBits);
 }
 
 function littleEndian(value, nbBits) {
