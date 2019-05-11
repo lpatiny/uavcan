a5e70373abca2763444f12aa4fd556a400376f5c
Luc Patiny <luc@patiny.com>
2019-05-11 10:40:21 +0200

    remove useless parser files

--- src/parser/__tests__/parse.gps.js
+++ src/parser/__tests__/parse.gps.js
@@ -9,6 +9,15 @@ describe('uavtest gps', () => {
     let data = [12, 14, 15];
 
     let result = parse(data, kindGeo);
-    expect(result).toMatchSnapshot();
+    expect(result).toEqual({
+      gimbalId: 12,
+      heightCm: 0,
+      heightReference: 0,
+      latitudeDeg1E7: 0,
+      longitudeDeg1E7: 15,
+      mode: {
+        commandMode: 14
+      }
+    });
   });
 });
