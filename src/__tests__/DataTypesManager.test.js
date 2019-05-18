138afa6bf622a955a5262bc1dbe8c603ff5779e1
Luc Patiny <luc@patiny.com>
2019-05-18 08:50:23 +0200

    add testcase for DataTpesManager

---
+++ src/__tests__/DataTypesManager.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const DataTypesManager = require('../DataTypesManager');
+
+describe('DataTypesManager', () => {
+  it('get service by ID', () => {
+    let dataType = DataTypesManager.getServiceByID(1);
+    expect(dataType.id).toBe('uavcan.protocol.GetNodeInfo');
+  });
+
+  it('get message by ID', () => {
+    let dataType = DataTypesManager.getMessageByID(1);
+    expect(dataType.id).toBe('uavcan.protocol.dynamic_node_id.Allocation');
+  });
+});
